40ae5a601facdc76edf863ac3997a69ebd18310a
zjt <jintao7080@gmail.com>
2017-08-26 15:50:10 +0800

    lengthOfLongestSubstring

---
+++ lengthOfLongestSubstring.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
+	fmt.Println(lengthOfLongestSubstring("bbb"))
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+	fmt.Println(lengthOfLongestSubstring("dvdf"))
+	fmt.Println(lengthOfLongestSubstring("abba"))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	var dict [256]int
+	var maxLen int = 0
+	var start int = -1
+	for i := 0; i != len(s); i++ {
+		if dict[s[i]] > start {
+			start = dict[s[i]]
+		}
+		dict[s[i]] = i
+		if i-start > maxLen {
+			maxLen = i - start
+		}
+	}
+	fmt.Println(dict)
+	return maxLen
+}
